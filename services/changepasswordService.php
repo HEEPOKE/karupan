5fbf7607f130150a132c4293ab4fc07896c47470
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-03-29 11:15:05 +0700

    a

--- services/changepasswordService.php
+++ services/changepasswordService.php
@@ -3,7 +3,7 @@ session_start();
 require('../database/clinet.php');
 
 if (count($_POST) > 0) {
-    $result = mysqli_query($conn, "SELECT *from account WHERE username='" . $_SESSION["username"] . "'");
+    $result = mysqli_query($conn, "SELECT *FROM account WHERE username='" . $_SESSION["username"] . "'");
     $row = mysqli_fetch_array($result);
     if ($_POST["newpassword"] == $row["password"]) {
         mysqli_query($conn, "UPDATE username set password='" . $_POST["newpassword"] . "' WHERE userId='" . $_SESSION["username"] . "'");
