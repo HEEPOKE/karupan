382b691cf778d15b7f3d488492a0f13072ef2e93
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-03-31 15:26:58 +0700

    6

--- services/changepasswordService.php
+++ services/changepasswordService.php
@@ -3,16 +3,16 @@ session_start();
 require('../database/clinet.php');
 
 $username = $_POST['username'];
-$newpassword = $_POST['newpassword'];
+
 
 $sql = "UPDATE `account` 
-SET `username`='$username',
-`password`='$password',
-WHERE `password`='" . $_SESSION["usernames"] . "' AND `username`='" . $_SESSION["passwords"] . "'";
+-- SET `username`='$username',
+-- `password`='$password',
+WHERE `username`='" . $_SESSION["usernames"] . "' AND `username`='" . $_SESSION["passwords"] . "'";
 
 
 if ($conn->query($query) == TRUE) {
-    echo '<script>alert("เปลี่ยนรหัสผ่านสำเร็จ")</script>';
+    echo '$sql';
 } else {
     echo '<script>alert("รหัสผ่านไม่ตรงกัน")</script>';
 }
