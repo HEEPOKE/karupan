6225b10220a6d9575064850d29b75018ddee209c
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-04-01 09:08:18 +0700

    2

--- database/clinet.php
+++ database/clinet.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "id16798641_projects";
-$password = "Qwertyui-12345678";
-$dbname = "id16798641_project";
+$username = "root";
+$password = "root";
+$dbname = "localhost";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
